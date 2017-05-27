05d7ce1f334f578a8c09e299bfc1cd0f6184d901
Yanahma <evilbottom1@hotmail.co.uk>
2017-05-27 21:00:15 +0100

    Chapter 6 commit

---
+++ Chapter 6/Puzzle League (Chapter 6)/Utilities/ContentHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PuzzleLeague.Utilities
+{
+   /// <summary>
+   /// Helper class in order to contain all loaded content, and provide static access to it
+   /// </summary>
+   public static class ContentHelper
+   {
+      // The dictionary that holds the string/texture combinations
+      private static Dictionary<string,Texture2D> textures;
+
+      // Static constructor
+      static ContentHelper()
+      {
+         textures = new Dictionary<string, Texture2D>();
+      }
+
+      /// <summary>
+      /// Method for adding a texture to the contenthelper
+      /// </summary>
+      /// <param name="name">The name of this texture (same as content name)</param>
+      /// <param name="texture">The loaded texture (use Main.Content to load)</param>
+      public static void AddTexture(string name, Texture2D texture)
+      {
+         if (!(textures.ContainsKey(name)))
+            textures.Add(name, texture);
+         else
+            throw new Exception("Attempting to add a texture twice in ContentHelper");
+      }
+
+      /// <summary>
+      /// Method to access a loaded texture from a given string name
+      /// </summary>
+      /// <param name="name">The name of this texture (same as content name)</param>
+      /// <returns>The associated texture</returns>
+      public static Texture2D GetTexture(string name)
+      {
+         if (textures.ContainsKey(name))
+            return textures[name];
+         else
+            throw new IndexOutOfRangeException("Attempting to retrieve a Texture2D that isn't loaded in ContentHelper");
+      }
+   }
+}
---
+++ Chapter 6/Puzzle League (Chapter 6)/Utilities/RandomHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleLeague.Utilities
+{
+   /// <summary>
+   /// Helper class in order to provide static access to a single "Random" object
+   /// </summary>
+   class RandomHelper
+   {
+      // Private, static random object
+      private static Random random;
+
+      // Private, static constructor to initialize the random object
+      static RandomHelper()
+      {
+         random = new Random();
+      }
+
+
+      /// <summary>
+      /// Static access to "Next" function
+      /// </summary>
+      /// <returns>A random integer</returns>
+      public static int Next() => random.Next();
+
+      /// <summary>
+      /// Static access to "Next" function with a maximum value
+      /// </summary>
+      /// <param name="max">The maximum value of the random integer</param>
+      /// <returns>A random integer within the selected range</returns>
+      public static int Next(int max) => random.Next(max);
+
+      /// <summary>
+      /// Static access to "Next" function with a min/max range
+      /// </summary>
+      /// <param name="min">The minimum value of the random integer</param>
+      /// <param name="max">The maximum value of the random integer</param>
+      /// <returns>A random integer within the selected range</returns>
+      public static int Next(int min, int max) => random.Next(min, max);
+   }
+}
