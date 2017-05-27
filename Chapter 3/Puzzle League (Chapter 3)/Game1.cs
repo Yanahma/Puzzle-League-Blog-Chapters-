288f5a290da1b8f449961fc2e6536eb4c75469ad
Yanahma <evilbottom1@hotmail.co.uk>
2017-05-27 09:43:47 +0100

    Chapter 3 commit

---
+++ Chapter 3/Puzzle League (Chapter 3)/Game1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using PuzzleLeague.Utilities;
+
+namespace PuzzleLeague
+{
+   /// <summary>
+   /// This is the main type for your game.
+   /// </summary>
+   public class Game1 : Game
+   {
+      GraphicsDeviceManager graphics;
+      SpriteBatch spriteBatch;
+
+      public Game1()
+      {
+         graphics = new GraphicsDeviceManager(this);
+         Content.RootDirectory = "Content";
+      }
+
+      /// <summary>
+      /// Allows the game to perform any initialization it needs to before starting to run.
+      /// This is where it can query for any required services and load any non-graphic
+      /// related content.  Calling base.Initialize will enumerate through any components
+      /// and initialize them as well.
+      /// </summary>
+      protected override void Initialize()
+      {
+         graphics.PreferredBackBufferWidth = 1280;
+         graphics.PreferredBackBufferHeight = 720;
+         //graphics.PreferredBackBufferWidth = 800;
+         //graphics.PreferredBackBufferHeight = 600;
+         graphics.ApplyChanges();
+
+         // Update ScaleHelper static class as well
+         ScaleHelper.UpdateBufferValues(graphics.PreferredBackBufferHeight, graphics.PreferredBackBufferWidth);
+
+         base.Initialize();
+      }
+
+      /// LoadContent will be called once per game and is the place to load
+      /// all of your content.
+      protected override void LoadContent()
+      {
+         // Create a new SpriteBatch, which can be used to draw textures. 
+         spriteBatch = new SpriteBatch(GraphicsDevice);
+
+         // Load textures into ContentHelper
+         ContentHelper.AddTexture("tileBlue_27", Content.Load<Texture2D>("Graphics\\tileBlue_27"));
+         ContentHelper.AddTexture("tileBlue_31", Content.Load<Texture2D>("Graphics\\tileBlue_31"));
+         ContentHelper.AddTexture("tileGreen_27", Content.Load<Texture2D>("Graphics\\tileGreen_27"));
+         ContentHelper.AddTexture("tileGreen_35", Content.Load<Texture2D>("Graphics\\tileGreen_35"));
+         ContentHelper.AddTexture("tilePink_27", Content.Load<Texture2D>("Graphics\\tilePink_27"));
+         ContentHelper.AddTexture("tilePink_30", Content.Load<Texture2D>("Graphics\\tilePink_30"));
+         ContentHelper.AddTexture("tileRed_27", Content.Load<Texture2D>("Graphics\\tileRed_27"));
+         ContentHelper.AddTexture("tileRed_36", Content.Load<Texture2D>("Graphics\\tileRed_36"));
+         ContentHelper.AddTexture("tileYellow_27", Content.Load<Texture2D>("Graphics\\tileYellow_27"));
+         ContentHelper.AddTexture("tileYellow_33", Content.Load<Texture2D>("Graphics\\tileYellow_33"));
+      }
+
+      /// <summary>
+      /// UnloadContent will be called once per game and is the place to unload
+      /// game-specific content.
+      /// </summary>
+      protected override void UnloadContent()
+      {
+         // TODO: Unload any non ContentManager content here
+      }
+
+      int count = 0;
+      List<Row> rows = new List<Row>();
+      /// <summary>
+      /// Allows the game to run logic such as updating the world,
+      /// checking for collisions, gathering input, and playing audio.
+      /// </summary>
+      protected override void Update(GameTime gameTime)
+      {
+         count++;
+         if (count == 53)
+         {
+            rows.Add(Row.RandomRow());
+            count = 0;
+         }
+         foreach (Row r in rows)
+            r.Update();
+
+         base.Update(gameTime);
+      }
+
+      /// This is called when the game should draw itself.
+      protected override void Draw(GameTime gameTime)
+      {
+         GraphicsDevice.Clear(Color.CornflowerBlue);
+
+         spriteBatch.Begin();
+         foreach (Row r in rows)
+            r.Draw(spriteBatch);
+
+         spriteBatch.End();
+
+         base.Draw(gameTime);
+      }
+   }
+}
---
+++ Chapter 3/Puzzle League (Chapter 3)/Row.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PuzzleLeague
+{
+
+   /// <summary>
+   /// Class that describes a row of blocks on the GameBoard
+   /// </summary>
+   class Row
+   {
+      // Array of blocks that belong to this row (0 = leftmost, 5 = rightmost)
+      private Block[] blocks;
+
+      // Defines the X,Y position of this Row
+      private Point position;
+
+      // Public accessor for 'position' field 
+      public Point Position { get { return position; } }
+
+      // Constructor
+      public Row()
+      {
+         blocks = new Block[6];
+      }
+
+      // Add a block to the "blocks" array
+      public void AddBlock(Block block, int index) => blocks[index] = block;
+
+      // Accessability for Blocks to know which position they are in
+      public int IndexOfBlock(Block item) => Array.IndexOf(blocks, item);
+
+      // Main update method
+      public void Update()
+      {
+         position.Y += 1;
+         foreach (Block b in blocks)
+            b.Update();
+      }
+
+      // Main draw method
+      public void Draw(SpriteBatch spriteBatch)
+      {
+         foreach (Block b in blocks)
+            b.Draw(spriteBatch);
+      }
+
+      // Create a random row of blocks
+      static public Row RandomRow()
+      {
+         Row rndRow = new Row();
+         for (var i = 0; i <= 5; i++)
+         {
+            rndRow.AddBlock(Block.RndBlockExcEmpty(rndRow), i);
+         }
+         return rndRow;
+      }
+   }
+}
